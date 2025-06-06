366afba7b6f3a575682e55b635f1f0e66c049b19
prophetouw <10101bigboss10101@gmail.com>
2025-06-06 12:58:27 -0500

    pc

--- Packages/_Index/prophetouw_faye@1.2.2/Signal.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent.Parent["prophetouw_simplesignal@1.0.12"]["simplesignal"])
---
+++ Packages/_Index/prophetouw_faye@1.3.1/Signal.lua
@@ -0,0 +1,1 @@
+return require(script.Parent.Parent["prophetouw_simplesignal@1.0.13"]["simplesignal"])
